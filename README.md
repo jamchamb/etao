eca2349bcef922d2e61d0f20dd6ae421ab438a28
James Chambers <jameschambers2@gmail.com>
2016-02-29 23:43:52 -0500

    Add readme and license

---
+++ README.md
@@ -0,0 +1,8 @@
+etao
+====
+[![PyPI version](https://badge.fury.io/py/etao.svg)](https://badge.fury.io/py/etao)
+
+Simple cryptanalysis library.
+
+# Installation
+`pip install etao`
--- setup.py
+++ setup.py
@@ -1,10 +1,25 @@
 from setuptools import setup
+import pypandoc
 
-setup(name='etao',
-      version='0.0.0',
-      description='Simple cryptanalysis library',
-      url='https://github.com/jamchamb/etao',
-      author='James Chambers',
-      author_email='jameschambers2@gmail.com',
-      packages=['etao'],
-      zip_safe=False)
+long_description = pypandoc.convert('README.md', 'rst')
+long_description = long_description.replace('\r', '')
+
+setup(
+    name='etao',
+    version='0.1.0',
+
+    description='Simple cryptanalysis library',
+    long_description=long_description,
+
+    url='https://github.com/jamchamb/etao',
+
+    author='James Chambers',
+    author_email='jameschambers2@gmail.com',
+
+    license='GPLv3',
+
+    keywords='cryptography cryptanalysis ciphers',
+
+    packages=['etao'],
+    zip_safe=False
+)
