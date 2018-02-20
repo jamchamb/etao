6ed0ec1f4e56626d10d5be0ff43713882d043e05
James Chambers <james.chambers@nccgroup.trust>
2018-02-19 20:08:12 -0500

    remove extra newlines

--- examples/score_filter.py
+++ examples/score_filter.py
@@ -2,6 +2,7 @@
 # Filter text by ngram score from stdin
 import argparse
 import fileinput
+import sys
 from etao import NgramFrequencyScorer
 from etao.freq import ENGLISH_DIGRAMS
 
@@ -17,7 +18,7 @@ def main():
     for line in fileinput.input(args.rest):
         score = scorer.score(line)
         if score > args.cutoff:
-            print line
+            sys.stdout.write(line)
 
 
 if __name__ == "__main__":
