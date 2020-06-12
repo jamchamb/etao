19c45f8256da21dd8f848272bb38f68a4924ebb6
James Chambers <jameschambers2@gmail.com>
2020-06-12 18:28:17 -0400

    emoji buffer preview

--- etao/util.py
+++ etao/util.py
@@ -28,3 +28,15 @@ def contains_nonprintables(input_string):
         if char not in PRINTABLE:
             return True
     return False
+
+
+def preview_crack_buf(cracked, unknown_char=u"\U0001F47B"):
+    """Print out array of characters where unknown values are
+    represented by None"""
+    preview = ''
+    for j in range(len(cracked)):
+        if cracked[j] is None:
+            preview += unknown_char
+        else:
+            preview += chr(cracked[j])
+    return preview
